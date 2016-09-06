32543246bd34ec18ea61b3b07e5439459d8b9cb9
Robson Correia <robsoncorreia01@outlook.com>
2016-09-06 17:11:36 -0300

    s

---
+++ SistemaEstacionamento/SistemaEstacionamento/Carro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaEstacionamento
+{
+    class Carro
+    {
+        public string porte;
+        public string marca;
+        public string modelo;
+        private string placa;
+
+
+        public void Exibir()
+        {
+            Console.WriteLine("Marca {0}\nModelo {1},\nPlaca {2}, Porte {3}", this.marca ,this.modelo, this.placa,this.porte);
+        }
+    }
+}
---
+++ SistemaEstacionamento/SistemaEstacionamento/Estacionamento.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SistemaEstacionamento
+{
+    class Estacionamento 
+    {
+        public byte carrosEstacionadosNoDia;
+        private byte carrosEstacionadosNoMomento;
+        public byte vagasDisponiveis;
+        public float total;
+
+        public string porte;
+        public string marca;
+        public string modelo;
+        private string placa;
+
+        public void RegistrarEntrada(object novoCarro)
+        {
+            Console.WriteLine("Registrando a entrada do veículo:\nMarca {0}\nModelo {1},\nPlaca {2}, Porte {3}", this.marca, this.modelo, this.placa, this.porte);
+            carrosEstacionadosNoMomento++;
+            carrosEstacionadosNoDia++;
+            vagasDisponiveis--;
+        }
+        public void RegistrarSaida(object novaCarro)
+        {
+            Console.WriteLine("Registrando a saida do veículo:\nMarca {0}\nModelo {1},\nPlaca {2}, Porte {3}", this.marca, this.modelo, this.placa, this.porte);
+            carrosEstacionadosNoMomento--;
+            vagasDisponiveis++;
+
+            if (this.porte == "P")
+            {
+                total = 10;
+            }
+            else if (this.porte == "M")
+            {
+                total = 15;
+            }
+            else if (this.porte == "G")
+            {
+                total = 20;
+            }
+        }
+    }
+}
